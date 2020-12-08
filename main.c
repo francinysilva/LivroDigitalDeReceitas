ecfaa1c4d25ea6e200d93fadba50dbde03b85bd0
francinysilva <74836063+francinysilva@users.noreply.github.com>
2020-12-08 15:33:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 #include "lib.h"
 
-
 int main(){
 	
-	int i=0,nReceita;
+//	int i=0,nReceita;
 	
-	while(i==0){	
-		mostraLista();
-		scanf("%d", &nReceita);
-		if(confirmaEscolha(nReceita)=='s'){
-			mostraReceita(nReceita);
-			i++;
-		}
-	}
+//	while(i==0){	
+//		mostraLista();
+//		scanf("%d", &nReceita);
+//		if(confirmaEscolha(nReceita)=='s'){
+//			mostraReceita(nReceita);
+//			i++;
+//		}
+//	}
+	
+	mostraListaRecursiva(0);
 	
 	return 0;
 }
