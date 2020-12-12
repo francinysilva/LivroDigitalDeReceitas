6498d608b0b399d1c72403fc2aac1543d7fe29d2
francinysilva <74836063+francinysilva@users.noreply.github.com>
2020-12-12 20:35:31 -0300

    Update fran.h

--- fran.h
+++ fran.h
@@ -11,12 +11,3 @@ void alteraRendimento(int n, char *str_rend, int *ptr_vet);
 void quantPorRendimento(int rend, int n, int *ptr_quant);
 
 #endif 
-
-/* 
-2 - Alem disso, a tua funcao recursiva apenas mostra outra receita de forma bastante questionavel.
-Caso o usuario queria ver N receitas, havera N instancias de mostraListaRecursiva() na memoria.
-
-La tem a receita base, mas se o usuario quiser ajustar a receita ao numero de pessoas, o site ajusta sozinho a proporcao dos ingredientes. Muito legal.
-Se tu colocar essa funcionalidade no teu site, poderas criar funcoes com passagem por referencia, teras oportunidade de trabalhar com strings e finalmente,
- uma funcao recursiva pode sair de forma mais funcional.
-*/
