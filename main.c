9577f6d73645fe3ba58d7dc9e8fbe19efeaa5970
francinysilva <74836063+francinysilva@users.noreply.github.com>
2020-12-12 20:31:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lib.h"
+
 int main(){
 	
-//	int i=0,nReceita;
+	/*int i=0,nReceita;
+	int vetSuspiro[9] = {2,40,1,6,8,140,140,15,20};
+	int vetMassaCaseira[5] = {3,400,4,3,30};
+	int vetPesto[7] = {4,1,1,3,100,1,2};
+	int vetFarofaPinhao[9] = {1,1,1,1,2,1,1,1,2};
+	int vetPeixeAssado[8] = {1,2,2,2,15,200,2,2};
 	
-//	while(i==0){	
-//		mostraLista();
-//		scanf("%d", &nReceita);
-//		if(confirmaEscolha(nReceita)=='s'){
-//			mostraReceita(nReceita);
-//			i++;
-//		}
-//	}
+	
+	while(i==0){	
+		mostraLista();
+		scanf("%d", &nReceita);
+		if(confirmaEscolha(nReceita)=='s'){
+			switch(nReceita){
+				case 1:
+					mostraReceita(nReceita, vetSuspiro);
+					break;
+				case 2:
+					mostraReceita(nReceita, vetMassaCaseira);
+					break;
+				case 3:
+					mostraReceita(nReceita, vetPesto);
+					break;
+				case 4:
+					mostraReceita(nReceita, vetFarofaPinhao);
+					break;
+				case 5:
+					mostraReceita(nReceita, vetPeixeAssado);
+					break;
+			}
+			i++;
+		}
+	}*/
 	
 	mostraListaRecursiva(0);
 	
