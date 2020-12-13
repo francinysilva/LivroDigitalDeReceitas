6ef2a2e46e982acbf6a4ae20645fdb1d558e0dc9
francinysilva <74836063+francinysilva@users.noreply.github.com>
2020-12-13 17:21:57 -0300

    Update fran.h

--- fran.h
+++ fran.h
@@ -1,13 +1,13 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
 
-void mostraLista (void);
-void mostraListaRecursiva(int i);
-char confirmaEscolha (int n);
-void mostraReceita(int n, int *ptr_vet);
-float mostraLucro(float n, int rend);
-float calculaLucro(float n);
-void alteraRendimento(int n, char *str_rend, int *ptr_vet);
-void quantPorRendimento(int rend, int n, int *ptr_quant);
+void mostraLista (void); //mostra a lista de receitas disponíveis e se repete até ter uma opção escolhida e confirmada
+void mostraListaRecursiva(int i); //mostra a lista de receitas disponíveis  e se repete de forma recursiva (chamando a si mesma) até ter uma opção escolhida e confirmada 
+char confirmaEscolha (int n); //pergunta ao usuário se ele realmente quer ver aquela receita ou quer escolher outra
+void mostraReceita(int n, int *ptr_vet);//recebe o código da receita escolhida e também o ponteiro vetor com a quantidade dos ingredientes da receita escolhida
+float mostraLucro(float n, int rend);//faz o cálculo do valor de custo sobre o rendimento da receita
+float calculaLucro(float n);//coloca a porcentagem de lucro no valor base de custo da receita
+void alteraRendimento(int n, char *str_rend, int *ptr_vet);//compara a leitura de string do usuário pra saber se ele quer alterar o rendimento da receita e chama a quantPorRendimento conforme a necessidade
+void quantPorRendimento(int rend, int n, int *ptr_quant);//recebe o código da alteraRendimento e o código da receita, e altera as quantidades da receita conforme p esperado
 
 #endif 
