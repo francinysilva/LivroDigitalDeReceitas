cc6a17a59358728b9cad64b58c09e8238905944c
francinysilva <74836063+francinysilva@users.noreply.github.com>
2021-01-08 11:29:01 -0300

    Update fran.c

--- fran.c
+++ fran.c
@@ -52,48 +52,50 @@ char confirmaEscolha(int nReceita){
 	}
 }
 
-void alteraRendimento(int n, char *str_rend, int *ptr_vet){
-	if(strcmp(str_rend,"metade")){
-		quantPorRendimento(1,n,ptr_vet);
-	}else if(strcmp(str_rend,"dobro")){
-		quantPorRendimento(2,n,ptr_vet);
-	}else if(strcmp(str_rend,"quadruplo")){
-		quantPorRendimento(3,n,ptr_vet);
-	}else{
-		exit(0);
+char confirmaEscolhaRec(int nReceita){
+	char res;
+	switch(nReceita){
+		case 1:
+			printf("#####SUSPIRO MERENGUE#####\n\nDescricao:\n");
+			printf("Deseja visualizar esta receita?s/n\n");
+			scanf(" %c", &res);
+			if(res == 's' || res == 'n'){
+				return res;	
+			}else{
+				confirmaEscolhaRec(nReceita);
+			}
+			break;
+		case 2:
+			printf("#####MASSA CASEIRA FRESCA#####\n\nDescricao:\n");
+			printf("Deseja visualizar esta receita?s/n\n");
+			scanf(" %c", &res);
+			if(res == 's' || res == 'n'){
+				return res;	
+			}else{
+				confirmaEscolhaRec(nReceita);
+			}
+			break;
+		case 3:
+			printf("#####MOLHO PESTO#####\n\nDescricao:\n");
+			printf("Deseja visualizar esta receita?s/n\n");
+			scanf(" %c", &res);
+			if(res == 's' || res == 'n'){
+				return res;	
+			}else{
+				confirmaEscolhaRec(nReceita);
+			}
+			break;
+		case 0:
+			exit(0);
 	}
 }
 
-void alteraRendimentoRec(int n, char *str_rend, int *ptr_vet, int aux){
+
+void alteraRendimento(int n, char *str_rend, int *ptr_vet){
 	if(strcmp(str_rend,"metade")){
-		quantPorRendimento(0,n,ptr_vet);
-		if(aux == 1){
-			printf("\nQuer mudar o rendimento? Digite: metade, dobro, quadruplo ou sair pra encerrar o programa:\n");
-			scanf(" %s", *str_rend);
-			alteraRendimentoRec(n, str_rend,ptr_vet, 1);
-		}else{
-			exit(0);	
-		}
+		quantPorRendimento(2,n,ptr_vet);
 	}else if(strcmp(str_rend,"dobro")){
 		quantPorRendimento(1,n,ptr_vet);
-		if(aux == 1){
-			printf("\nQuer mudar o rendimento? Digite: metade, dobro, quadruplo ou sair pra encerrar o programa:\n");
-			scanf(" %s", *str_rend);
-			alteraRendimentoRec(n, str_rend,ptr_vet, 1);
-		}else{
-			exit(0);	
-		}
-	}else if(strcmp(str_rend,"quadruplo")){
-		quantPorRendimento(2,n,ptr_vet);
-		if(aux == 1){
-			printf("\nQuer mudar o rendimento? Digite: metade, dobro, quadruplo ou sair pra encerrar o programa:\n");
-			scanf(" %s", *str_rend);
-			alteraRendimentoRec(n, str_rend,ptr_vet, 1);
-		}else{
-			exit(0);	
-		}
-	}else{
-		exit(0);
 	}
 }
 
@@ -104,7 +106,7 @@ void quantPorRendimento(int rend, int n, int *ptr_vet){
 			printf("%d claras de ovos\n%d g de acucar confeiteiro\n%d limao", *(ptr_vet+0)-1, *(ptr_vet+1)/2, *(ptr_vet+2));
 			break;
 		case 2:
-			printf("%d xicaras (cha) de farinha de trigo (cerca de %d g)\n%d ovos\n%d colheres (cha) de sal", *(ptr_vet+0)-2, *(ptr_vet+1)/2, *(ptr_vet+2)/2, *(ptr_vet+3));
+			printf("%d xicaras (cha) de farinha de trigo (cerca de %d g)\n%d ovos\n%d colheres (cha) de sal", *(ptr_vet+0)-2, *(ptr_vet+1)/2, *(ptr_vet+2)/2, *(ptr_vet+3)-1);
 			break;
 		case 3:
 			printf("%d dentes de alho\n%d colher (cha) de sal\n%d xicara (cha) de folhas de manjericao\n%d colheres (cha) de nozes\n%d g de queijo parmesao ralado\n%d xicara (cha) de azeite", *(ptr_vet+0)/2, *(ptr_vet+1), *(ptr_vet+2), *(ptr_vet+3), *(ptr_vet+4)/2, *(ptr_vet+5), *(ptr_vet+6)*2);
@@ -123,20 +125,6 @@ void quantPorRendimento(int rend, int n, int *ptr_vet){
 			printf("%d dentes de alho\n%d colher (cha) de sal\n%d xicara (cha) de folhas de manjericao\n%d colheres (cha) de nozes\n%d g de queijo parmesao ralado\n%d xicara (cha) de azeite", *(ptr_vet+0)*2, *(ptr_vet+1)*2, *(ptr_vet+2)*2, *(ptr_vet+3)*2, *(ptr_vet+4)*2, *(ptr_vet+5));
 			break;
 		}
-	}else if(rend==3){
-		switch(n){
-		case 1:
-			printf("%d claras de ovos\n%d g de acucar confeiteiro\n%d limao", *(ptr_vet+0)*4, *(ptr_vet+1)*4, *(ptr_vet+2)*4);
-			break;
-		case 2:
-			printf("%d xicaras (cha) de farinha de trigo (cerca de %d g)\n%d ovos\n%d colheres (cha) de sal", *(ptr_vet+0)*4, *(ptr_vet+1)*4, *(ptr_vet+2)*4, *(ptr_vet+3)*4);
-			break;
-		case 3:
-			printf("%d dentes de alho\n%d colher (cha) de sal\n%d xicara (cha) de folhas de manjericao\n%d colheres (cha) de nozes\n%d g de queijo parmesao ralado\n%d xicara (cha) de azeite", *(ptr_vet+0)*4, *(ptr_vet+1)*4, *(ptr_vet+2)*4, *(ptr_vet+3)*4, *(ptr_vet+4)*4, *(ptr_vet+5)*2);
-			break;
-		}
-	}else{
-		exit(0);
 	}
 }
 
@@ -164,9 +152,9 @@ void mostraReceita(int n, int *ptr_vet, char *ptr_receita, char *ptr_passos){
 			printf("\n\nRendimento: Rende %d unidades", vet_rend[i]);
 			printf("\nCusto medio: %.2f reais", vet_custo[i]);
 			printf("\nValor p/ venda com porcentagem de lucro dividido por quantidade de rendimento: %.2f reais\n\n", mostraLucro(calculaLucro(vet_custo[i]), vet_rend[i]));
-			printf("Quer mudar o rendimento? Digite: metade, dobro, quadruplo, ou sair pra encerrar o programa:\n");
+			printf("Digite metade ou dobro pra descobrir a qntde de ingredientes necessarios de acordo com a tua necessidade de rendimento:\n");
 			scanf(" %s", str_rend);
-			alteraRendimentoRec(n, str_rend,ptr_vet, 1);
+			alteraRendimento(n, str_rend, ptr_vet);
 			break;
 		case 2:
 			printf("%s", ptr_receita);
@@ -178,9 +166,9 @@ void mostraReceita(int n, int *ptr_vet, char *ptr_receita, char *ptr_passos){
 			printf("\n\nRendimento: Rende %d unidades", vet_rend[i]);
 			printf("\nCusto medio: %.2f reais", vet_custo[i]);
 			printf("\nValor p/ venda com porcentagem de lucro dividido por quantidade de rendimento: %.2f reais\n\n", mostraLucro(calculaLucro(vet_custo[i]), vet_rend[i]));
-			printf("Quer mudar o rendimento? Digite: metade, dobro, quadruplo, ou sair pra encerrar o programa:\n");
+			printf("Digite metade ou dobro pra descobrir a qntde de ingredientes necessarios de acordo com a tua necessidade de rendimento:\n");
 			scanf(" %s", str_rend);
-			alteraRendimentoRec(n, str_rend,ptr_vet, 1);
+			alteraRendimento(n, str_rend, ptr_vet);
 			break;
 		case 3:
 			printf("%s", ptr_receita);
@@ -192,9 +180,9 @@ void mostraReceita(int n, int *ptr_vet, char *ptr_receita, char *ptr_passos){
 			printf("\n\nRendimento: Rende %d unidades", vet_rend[i]);
 			printf("\nCusto medio: %.2f reais", vet_custo[i]);
 			printf("\nValor p/ venda com porcentagem de lucro dividido por quantidade de rendimento: %.2f reais\n\n", mostraLucro(calculaLucro(vet_custo[i]), vet_rend[i]));
-			printf("Quer mudar o rendimento? Digite: metade, dobro, quadruplo, ou sair pra encerrar o programa:\n");
+			printf("Digite metade ou dobro pra descobrir a qntde de ingredientes necessarios de acordo com a tua necessidade de rendimento:\n");
 			scanf(" %s", str_rend);
-			alteraRendimentoRec(n, str_rend,ptr_vet, 1);
+			alteraRendimento(n, str_rend, ptr_vet);
 			break;
 	}
 	fclose(txt);
